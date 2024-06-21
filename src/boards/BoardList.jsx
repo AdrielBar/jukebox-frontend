9d53ced3059235c99f1e5b19170993bdd5774906
AdrielBar <adrielbarzola@ufl.edu>
2024-06-21 17:14:52 -0400

    Styled the 'Board 1' Link

--- src/boards/BoardList.jsx
+++ src/boards/BoardList.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
+import './BoardList.css'
 
 export const BoardList = () => {
   return (
     <div>
       <ul>
         <li>
-          <Link to="/boards/board-1">Board 1</Link>
+          <h1 className="board-1-title">
+            <Link to="/boards/board-1">Board 1</Link>
+          </h1>
         </li>
       </ul>
     </div>
